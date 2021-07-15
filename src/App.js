43e362ec14d797e4becab4202a8b764f6554ab9e
Muhamad Fauzan <47935196+fauzannmw@users.noreply.github.com>
2021-07-16 03:13:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import { Container, Row } from "react-bootstrap";
 
 function App() {
+  const [currencyData, setCurrency] = useState({});
+  useEffect(() => {
+    fetch(
+      "http://data.fixer.io/api/latest?access_key=69d8a8a941e00f40e4ef291063c57c47&format=1&symbols=CAD,IDR,JPY,CHF,EUR,USD"
+      // data dalam bentuk object
+    )
+      .then((res) => res.json())
+      .then((data) => setCurrency(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Row>
+        <div className="currency-title">
+          <h1 className="currency-price">We Buy</h1>
+          <h1 className="currency-rate">Exchange Rate</h1>
+          <h1 className="currency-price">We Sell</h1>
+        </div>
+      </Row>
+      <hr />
+      {currencyData.rates &&
+        Object.keys(currencyData.rates).map((rateKey) => (
+          <Row key={rateKey}>
+            <div className="currency-data">
+              <h1 className="currency-price">{rateKey}</h1>
+              <h1 className="currency-price">
+                {currencyData.rates[rateKey] * 1.04}
+              </h1>
+              <h1 className="currency-price">{currencyData.rates[rateKey]} </h1>
+              <h1 className="currency-price">
+                {currencyData.rates[rateKey] * 0.96}
+              </h1>
+            </div>
+          </Row>
+        ))}
+    </Container>
   );
 }
 
